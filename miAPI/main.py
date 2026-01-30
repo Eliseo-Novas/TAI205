566e7a37e6baf2d21497a77c570a3747c0262015
Eliseo-Novas <123046572@upq.edu.mx>
2026-01-30 14:56:03 -0600

    Update main.py
    Práctica de FastAPI enfocada en la documentación automática de APIs utilizando Swagger y SwaggerUI. Incluye endpoints con parámetros obligatorios, opcionales, validaciones de tipos de datos y simulación de tiempos de espera.

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,14 +1,48 @@
 #1. importaciones 
 from fastapi import FastAPI
+from typing import Optional 
+import asyncio
 
 #2. Inicializacion APP
-app= FastAPI()
+app= FastAPI(title= ' MI Primer API',description="ELiseo Novas Echeverria", version='1.0.0')
+
+# BD FICTICIA 
+usuarios=[
+    {"id":"1","nombre":"Eliseo","edad":"20"},
+     {"id":"1","nombre":"Jesus","edad":"100000000000000"},
+      {"id":"1","nombre":"Julian","edad":"20"},
+]
 
 #3. Endpoints
-@app.get("/")
+@app.get("/", tags=['Inicio'])
 async def holaMundo () :
     return {"mensaje":"HOLA mundo FASTAPI"} 
 
-@app.get("/bienvenidos")
+@app.get("/v1/bienvenidos", tags=['Inicio'])
 async def bienvenidos () :
     return {"mensaje":"Bienvenidos"} 
+
+
+@app.get("/v1/promedio", tags=['Calificaciones'])
+async def promedio () :
+    await asyncio.sleep(3) #peticion, consultaBD..
+    return {"Calificacion":"10", 
+            "estatus":"200"
+            }
+
+@app.get("/v1/usuario{id}", tags=['Parametros'])
+async def consultaUno(id:int):
+    await asyncio.sleep(3) #peticion, consultaBD..
+    return {"Resultado":"usuario encontrado", "Estatus":"200"}
+
+
+@app.get("/v1/usuario_op/", tags=['Parametro opcional'])
+async def consultaOp(id:Optional[int]=None):
+    await asyncio.sleep(2) #peticion, consultaBD
+    if id is not None:
+        for usuario in usuarios:
+            if usuario["id"]==(id):
+                return {"Usuario encontrado":id,"Datos":usuario}
+            return {"Mensaje":"Usuario no encontrado"}
+    else:
+         return {"Aviso":"No se proporciono Id"}
